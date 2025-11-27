06fa9f01b42c39f94f62cbb6164a1edc2705edb0
mennymendoza <juanmendoza19971@gmail.com>
2025-11-26 22:24:36 -0800

    feat: added bufferline

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,20 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    keys = {
+      {
+        't',
+        function()
+          require('bufferline').cycle(1)
+        end,
+        desc = 'Cycle next tab',
+      },
+    },
+    opts = {},
+  },
+}
