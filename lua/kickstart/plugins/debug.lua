e1671274ccbc45c8f038b0b6f7a8ab1478431a40
mennymendoza <juanmendoza19971@gmail.com>
2025-11-26 22:05:48 -0800

    feat: removed potentially missing packages

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -94,7 +94,6 @@ return {
       -- online, please don't ask me how to install them :)
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
-        'delve',
       },
     }
 
